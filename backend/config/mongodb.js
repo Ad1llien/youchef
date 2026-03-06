fbac3750a58d0d74e3c46844f031b7cf92febd4a
akadilzhengisuly <akadilzh2004kz@gmail.com>
2026-03-06 21:05:28 +0500

    main backend functions

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,33 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//      mongoose.connection.on('connected', ()=> console.log("Database Connected"))
+//    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+// };
+
+// export default connectDB;
+import dns from 'dns';
+
+import dotenv from "dotenv";
+
 import mongoose from "mongoose";
 
+dotenv.config();
 const connectDB = async () => {
-      mongoose.connection.on('connected', ()=> console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+  console.log(process.env.MONGO_URI);
+  dns.setServers(['8.8.8.8', '8.8.4.4']);
+  try {
+    await mongoose.connect(
+      `${process.env.MONGODB_URI}`,
+      {
+        family: 4, // ensures IPv4 resolution
+      }
+    );
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
